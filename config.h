686c3d205e042c2af98338c392bf547b1e249615
sneweep <sneweep@github.com>
2022-03-22 18:09:22 -0400

    removed gaps

--- config.h
+++ config.h
@@ -3,10 +3,6 @@
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -92,13 +88,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_u,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-
- 	{ MODKEY|ControlMask,           XK_h,      incrgaps,       {.i = +1 } },
- 	{ MODKEY|ControlMask,           XK_l,      incrgaps,       {.i = -1 } },
- 	{ MODKEY|ControlMask,           XK_0,      togglegaps,     {0} },
- 	{ MODKEY|ControlMask|ShiftMask, XK_0,      defaultgaps,    {0} },
-
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+    { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
